c223c1b18809a255f44fe865f01ec2999877c104
Kayla Cross <kac288@georgetown.edu>
2018-07-23 21:23:51 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -9,7 +9,7 @@ class Application
 
     if req.path.match "/items/"
       item_name = req.path.split("/items/").last
-      @@items.find{|i| i.name == item_name}
+      item = @@items.find{|i| i.name == item_name}
     else
       resp.write "Item not found"
       resp.status = 404
